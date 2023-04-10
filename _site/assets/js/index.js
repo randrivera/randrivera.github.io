5003ba697e380d2150a107ed714a3012972e2698
randrivera <rand.k.rivera@gmail.com>
2023-04-10 14:54:30 -0400

    Flexed up the homepage + working colors

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -2,8 +2,29 @@ let txt = prompt("Welcome new friend!", "please enter your name here");
 let hello = document.getElementById("hello");
 let type = document.querySelector("#type-message")
 
+let rbackground = [
+    'FF48FF',
+    '004CE5',
+    '14c420',
+    'FFC200',
+    'ff4133',
+   
+  ];
+  
+  let r = document.querySelector(':root');
+  let randomColor = rbackground[Math.floor(Math.random()*rbackground.length)]
+  
+  
+  
+function myFunction_set() {
+    r.style.setProperty('--my-variable', '#'+ randomColor);	    
+
+}
+
 function helloMessage() {
+    myFunction_set();
     hello.innerHTML = "Hi " + txt + "! ♥";
 }
   
 window.onload = helloMessage();
+
