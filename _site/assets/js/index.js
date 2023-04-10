87116af47a003e6b95ea17e1cd104fa6ccc0f22d
randrivera <rand.k.rivera@gmail.com>
2023-04-10 14:55:39 -0400

    Updated homepagee

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -3,21 +3,23 @@ let hello = document.getElementById("hello");
 let type = document.querySelector("#type-message")
 
 let rbackground = [
-    'FF48FF',
-    '004CE5',
-    '14c420',
-    'FFC200',
-    'ff4133',
+    'F30909',
+    'F37E09',
+    '7EF309',
+    '0931F3',
+    'CB09F3',
    
   ];
   
   let r = document.querySelector(':root');
   let randomColor = rbackground[Math.floor(Math.random()*rbackground.length)]
+
   
   
   
 function myFunction_set() {
-    r.style.setProperty('--my-variable', '#'+ randomColor);	    
+    r.style.setProperty('--my-variable', '#'+ randomColor);	
+    // document.getElementById("background").value = "#"+randomColor;
 
 }
 
@@ -28,3 +30,33 @@ function helloMessage() {
   
 window.onload = helloMessage();
 
+
+// function setup(){
+//     createCanvas(windowWidth, windowHeight);
+//     colorPicker = select("#background");
+// }
+
+// function draw() {
+//     let root = document.documentElement;
+//     let color = colorPicker.value();
+//     root.style.setProperty('--my-variable', color);
+
+// }
+
+var colorPicker = new iro.ColorPicker("#picker", {
+    // Set the size of the color picker
+    width: 100,
+    // Set the initial color to pure red
+    color: "#" +randomColor,
+    layout: [
+        { 
+          component: iro.ui.Wheel,
+        },
+    ]
+  });
+
+  colorPicker.on('color:change', function(color) {
+    // log the current color as a HEX string
+    console.log(color.hexString);
+    r.style.setProperty('--my-variable', color.hexString);
+  });
