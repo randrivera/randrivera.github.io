80c239276f9daeb608b67abbf625abedbbc1507c
randrivera <rand.k.rivera@gmail.com>
2023-07-06 13:48:44 -0400

    updated stuff ig

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,38 +6,40 @@ let button = document.querySelector("#enterBtn");
 
 //hello message in header
 let hello = document.getElementById("hello");
-
 let r = document.querySelector(':root');
 
+r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
 
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+if (mediaQuery.matches){
+  //enter name interaction
+  button.addEventListener("click", function(){
+    //grab value from text input
+    let txt = document.querySelector("#collectName").value
+    //store value for userName
+    sessionStorage.setItem("userName", txt);
 
-button.addEventListener("click", function(){
-  //grab value from text input
-  let txt = document.querySelector("#collectName").value
-  //store value for userName
-  sessionStorage.setItem("userName", txt);
-
-  //change hello message name to have userName
-  hello.innerHTML = "Hi " + sessionStorage.getItem("userName") + "! ♥";
-
-  //hide nameScreen then display:none it afterwards so it does not interfere with other interactions
-  nameInput.style.opacity = "0";
-  setTimeout(function()
-    {nameInput.style.display = "none";
-    }, 1000);
-})
-
-//checks if a name is already stored and prevents the name popup if so
-function checkStorage(){
-  if (sessionStorage.getItem("userName")){
+    //change hello message name to have userName
     hello.innerHTML = "Hi " + sessionStorage.getItem("userName") + "! ♥";
-    nameInput.style.display = "none";
-  } else {
-    null
-  };
-}
 
-window.onload = checkStorage();
+    //hide nameScreen then display:none it afterwards so it does not interfere with other interactions
+    nameInput.style.opacity = "0";
+    setTimeout(function()
+      {nameInput.style.display = "none";
+      }, 1000);
+  })
+
+  //checks if a name is already stored and prevents the name popup if so
+  function checkStorage(){
+    if (sessionStorage.getItem("userName")){
+      hello.innerHTML = "Hi " + sessionStorage.getItem("userName") + "! ♥";
+      nameInput.style.display = "none";
+    } else {
+      null
+    };
+  } 
+  window.onload = checkStorage();
+};
 
 //change color on every click!
 let rbackground = [
@@ -48,24 +50,27 @@ let rbackground = [
   'CB09F3', 
 ];
 
-function randomNumberColor(){
 
-}
-
-  document.addEventListener("click", function(){
+//every click changes the key color
+document.addEventListener("click", function(){
+    //picks a random color from rbackground[]
     let randomColor = rbackground[Math.floor(Math.random()*rbackground.length)];
+    //sets the storage color as the random color from above
     sessionStorage.setItem("userColor", "#"+randomColor);
+    //make the variable color the stored color
     r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
-
   });
 
-  //dark + light mode code
-  document.documentElement.setAttribute("data-theme", sessionStorage.getItem("userMode"));
+//dark + light mode code
 
-  document.addEventListener("DOMContentLoaded", function(event) {
+//sets the mode to the stored userMode
+document.documentElement.setAttribute("data-theme", sessionStorage.getItem("userMode"));
+
+//checks for dark/light mode switches
+document.addEventListener("DOMContentLoaded", function(event) {
     let checkbox = document.querySelector('input[type="checkbox"]');
-    checkbox.addEventListener('change', function () {
 
+    checkbox.addEventListener('change', function () {
       let currentTheme = document.documentElement.getAttribute("data-theme");
       // Switch between `dark` and `light`
       let switchToTheme = currentTheme === "dark" ? "light" : "dark";
@@ -73,13 +78,14 @@ function randomNumberColor(){
 
       document.documentElement.setAttribute("data-theme", sessionStorage.getItem("userMode"));
     }
-  )});
+
+)});
   
 
-  // Christ (Chris Panicker - panicker.design) helped me with this one <3
-  // On the homepage, hovering over the title moves the corresponding preview image to the top
+// Christ (Chris Panicker - panicker.design) helped me with this one <3
+// On the homepage, hovering over the title moves the corresponding preview image to the top
   
-  // all the project previews
+// all the project previews
   let projects = [];
   projects[0] = document.querySelector("#projone");
   projects[1] = document.querySelector("#projtwo");
@@ -92,7 +98,7 @@ function randomNumberColor(){
   projects[8] = document.querySelector("#projnine");
 
 
-  //div in which all the preview images live in
+//div in which all the preview images live in
   let target = document.getElementById("target");
   let targetImg = document.querySelector("#target img");
   const scroller = document.querySelector(".Content");
@@ -100,21 +106,21 @@ function randomNumberColor(){
 
   for (let i=0; i<projects.length; i++){
     projects[i].addEventListener("mouseenter", function(){
-      // console.log(`${scroller.scrollTop}`);
+      console.log(`${scroller.scrollTop}`);
     
-      // console.log(`-${targetImg.clientHeight}`);
+      console.log(`-${targetImg.clientHeight}`);
       // subtract how much user has already scrolled on their own to the scroll distance
       target.style.top = `-${targetImg.clientHeight*i}` - `-${scroller.scrollTop}` + "px";
     });
 
     projects[i].addEventListener("mouseleave", function(){
     
-      // console.log(`-${targetImg.clientHeight}`);
+      console.log(`-${targetImg.clientHeight}`);
       target.style.top = "0px";
     });
   }
 
-  //following mouse eyes in nameScreen
+//following mouse eyes in nameScreen
   $(".nameInput").mousemove(function(event) {
     var eye = $(".eye");
     // console.log('eye', eye)
@@ -130,7 +136,7 @@ function randomNumberColor(){
     });
   });
 
-
+//award fireworks code
 const defaults = {
   spread: 120,
   ticks: 30,
@@ -140,7 +146,7 @@ const defaults = {
   shapes: ["star"],
 };
 
-
+//on hover, make fireworks!
  document.querySelectorAll('.awards').forEach(item => {
   item.addEventListener('mouseover', event => {
     console.log("confetti time");
@@ -161,6 +167,25 @@ const defaults = {
   });
   })
 });
+
+let arrows = document.querySelector(".arrows");
+let amIOn = false;
+
+arrows.addEventListener("click", function(){
+  let navMobile = document.querySelector(".navMobile");
+
+
+  if (amIOn == false){
+  navMobile.classList.add("show");
+  arrows.innerHTML="↑↑↑";
+  amIOn = true;
+  } else if (amIOn == true){
+  navMobile.classList.remove("show");
+  arrows.innerHTML="↓↓↓";
+  amIOn = false;
+  }
+  console.log(amIOn);
+})
 
 
 
--- assets/js/project.js
+++ assets/js/project.js
@@ -1,12 +1,23 @@
+//hello message in header
 let hello = document.getElementById("hello");
-let type = document.querySelector("#type-message")
-
-
+let r = document.querySelector(':root');
+//nameScreen
+let nameInput = document.querySelector(".nameInput");
+//enter button
+let button = document.querySelector("#enterBtn");
   
-  let r = document.querySelector(':root');
-  let nameInput = document.querySelector(".nameInput");
-  
-  hello.innerHTML = "Hi " + sessionStorage.getItem("userName") + "! ♥";
+//checks if a name is already stored and prevents the name popup if so
+function checkStorage(){
+  if (sessionStorage.getItem("userName")){
+    hello.innerHTML = "Hi " + sessionStorage.getItem("userName") + "! ♥";
+  } else {
+    null
+  };
+}
+
+window.onload = checkStorage();
+
+r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
 
 //change color on every click!
 let rbackground = [
@@ -17,26 +28,26 @@ let rbackground = [
   'CB09F3', 
 ];
 
-r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
+//every click changes the key color
+document.addEventListener("click", function(){
+  //picks a random color from rbackground[]
+  let randomColor = rbackground[Math.floor(Math.random()*rbackground.length)];
+  //sets the storage color as the random color from above
+  sessionStorage.setItem("userColor", "#"+randomColor);
+  //make the variable color the stored color
+  r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
+});
 
+//dark + light mode code
 
-  document.addEventListener("click", function(){
-    let randomColor = rbackground[Math.floor(Math.random()*rbackground.length)];
-    console.log("clicked");
-    sessionStorage.setItem("userColor", "#"+randomColor);
-    r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
+//sets the mode to the stored userMode
+document.documentElement.setAttribute("data-theme", sessionStorage.getItem("userMode"));
 
-  });
-
-  let targetDiv = document.querySelector(".marquee");
-
-  document.documentElement.setAttribute("data-theme", sessionStorage.getItem("userMode"));
-
-  document.addEventListener("DOMContentLoaded", function(event) {
-
+//checks for dark/light mode switches
+document.addEventListener("DOMContentLoaded", function(event) {
     let checkbox = document.querySelector('input[type="checkbox"]');
-    checkbox.addEventListener('change', function () {
 
+    checkbox.addEventListener('change', function () {
       let currentTheme = document.documentElement.getAttribute("data-theme");
       // Switch between `dark` and `light`
       let switchToTheme = currentTheme === "dark" ? "light" : "dark";
@@ -44,39 +55,60 @@ r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
 
       document.documentElement.setAttribute("data-theme", sessionStorage.getItem("userMode"));
     }
-  )});
+
+)});
 
 
-  const defaults = {
-    spread: 120,
-    ticks: 30,
-    gravity: 0,
-    decay: 0.99,
-    startVelocity: 50,
+
+//award fireworks code
+const defaults = {
+  spread: 120,
+  ticks: 30,
+  gravity: 0,
+  decay: 0.99,
+  startVelocity: 50,
+  shapes: ["star"],
+};
+
+//on hover, make fireworks!
+ document.querySelectorAll('.awards').forEach(item => {
+  item.addEventListener('mouseover', event => {
+    console.log("confetti time");
+  confetti({
+    ...defaults,
+    colors: sessionStorage.getItem("userColor"),
+    particleCount: 20,
+    scalar: 1.2,
     shapes: ["star"],
-  };
-  
-  
-   document.querySelectorAll('.awards').forEach(item => {
-    item.addEventListener('mouseover', event => {
-      console.log("confetti time");
-    confetti({
-      ...defaults,
-      colors: sessionStorage.getItem("userColor"),
-      particleCount: 20,
-      scalar: 1.2,
-      shapes: ["star"],
-    });
-  
-    confetti({
-      ...defaults,
-      colors: sessionStorage.getItem("userColor"),
-      particleCount: 10,
-      scalar: 0.75,
-      shapes: ["circle"],
-    });
-    })
+  });
+
+  confetti({
+    ...defaults,
+    colors: sessionStorage.getItem("userColor"),
+    particleCount: 10,
+    scalar: 0.75,
+    shapes: ["circle"],
+  });
   })
-  
-  
+});
+
+let arrows = document.querySelector(".arrows");
+let amIOn = false;
+
+arrows.addEventListener("click", function(){
+  let navMobile = document.querySelector(".navMobile");
+
+
+  if (amIOn == false){
+  navMobile.classList.add("show");
+  arrows.innerHTML="↑↑↑";
+  amIOn = true;
+  } else if (amIOn == true){
+  navMobile.classList.remove("show");
+  arrows.innerHTML="↓↓↓";
+  amIOn = false;
+  }
+  console.log(amIOn);
+})
+
 
