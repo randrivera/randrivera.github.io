4d34c56a53e2720d87bdb0de381957426e18a0c2
randrivera <rand.k.rivera@gmail.com>
2023-07-19 18:02:28 -0400

    added moving eyes

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,6 +59,7 @@ document.addEventListener("click", function(){
     sessionStorage.setItem("userColor", "#"+randomColor);
     //make the variable color the stored color
     r.style.setProperty('--my-variable', sessionStorage.getItem("userColor"));
+    updatePupil();
   });
 
 //dark + light mode code
@@ -120,22 +121,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
   }
 
-//following mouse eyes in nameScreen
-  $(".nameInput").mousemove(function(event) {
-    var eye = $(".eye");
-    // console.log('eye', eye)
-    var x = (eye.offset().left) + (eye.width() / 2);
-    var y = (eye.offset().top) + (eye.height() / 2);
-    var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = (rad * (180 / Math.PI) * -1) + 180;
-    eye.css({
-      '-webkit-transform': 'rotate(' + rot + 'deg)',
-      '-moz-transform': 'rotate(' + rot + 'deg)',
-      '-ms-transform': 'rotate(' + rot + 'deg)',
-      'transform': 'rotate(' + rot + 'deg)'
-    });
-  });
-
 //award fireworks code
 const defaults = {
   spread: 120,
@@ -186,6 +171,123 @@ arrows.addEventListener("click", function(){
   }
   console.log(amIOn);
 })
+
+let faces =[]
+// colour of background and canvas
+let backgroundColour = "#121212";
+let pupilColor = getComputedStyle(document.documentElement).getPropertyValue('--my-variable');
+console.log(pupilColor);
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  for(var i = 0; i < windowWidth/100 ; i++){
+    for(var y = 0; y < windowHeight/100 ; y++){
+      faces.push(new Face(i*150 + 90, y*150 + 90))
+    }
+  }
+  
+}
+
+function draw() {
+  background(backgroundColour);
+  faces.forEach(face => {
+    face.draw()
+  })
+  
+}
+
+function Face(x,y){
+  this.x = x;
+  this.y = y;
+  this.blinkPause = random(500,4000)
+  this.rightEye = new Eye(this.x + 20, y, this.blinkPause)
+  this.leftEye = new Eye(this.x- 20, y, this.blinkPause)
+  this.draw = function(){
+    this.rightEye.draw()
+    this.rightEye.blink()
+    this.leftEye.draw()
+    this.leftEye.blink()
+    
+  }
+}
+
+
+function Eye(x,y, binkPause){
+  this.x = x;
+  this.y = y;
+  this.d = 35; // diameter of circle
+  this.topLidY = this.y
+  this.dy = 1;
+  this.distance = 0;
+  this.angle = 0;
+  this.blinkPause = 0 // duration till next bink
+  this.topLidYOrigin = this.y // original position before animation
+  this.bottomLidY = this.y - this.d
+  this.blink = function() {
+    
+    // decrement blink pause duration
+    if(this.blinkPause > 0){
+      this.blinkPause -= 1
+      // return function to exit function early
+      return
+    }
+    
+      
+    if(this.topLidY >= this.topLidYOrigin + this.d /2 ){
+      this.blinkPause = binkPause
+      this.dy = -this.dy
+    }else if(this.topLidY < this.topLidYOrigin){
+      this.dy = -this.dy
+    }
+    
+    // animate eyelids
+    this.topLidY += this.dy
+    this.bottomLidY -= this.dy;
+  },
+    
+  this.draw = function(){
+    // eye ball
+    noStroke()
+    fill("white")
+    circle(this.x,this.y, this.d)
+    
+    // pupil
+    push();
+    fill(pupilColor)
+    // distance from mouse to eyeball center
+    this.distance =   constrain(int(dist(this.x,this.y,mouseX,mouseY)), 0, 500)
+    // map distance value from mouse position over eyeball radius
+    this.eyePos = map(this.d /3 , 0,500,0,this.distance)
+    this.angle = atan2(mouseY - this.y, mouseX - this.x);
+    translate(this.x, this.y);
+    rotate(this.angle);
+    // circle( distance from eye center, offset from angle, circe diameter)
+    circle(this.eyePos, 0, this.d / 2.5);
+    pop();
+  
+    // eye lids
+    fill(backgroundColour)
+    // stroke("red") // for debugging
+    rect(this.x - this.d/2, this.topLidY,  this.d, this.d)
+    rect(this.x - this.d/2, this.bottomLidY,  this.d, this.d)
+   
+    // eyeliner
+    noFill()
+    strokeWeight(3)
+    stroke("black")
+    circle(this.x,this.y, this.d)
+  }
+}
+  
+  function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+  
+  function updatePupil() {
+    pupilColor = sessionStorage.getItem("userColor");
+  }
+
 
 
 
