d5116e1d612c94d7cad90d8391172fb9601425e2
randrivera <rand.k.rivera@gmail.com>
2023-12-25 18:52:48 -0500

    mobile updates

--- _site/assets/js/project.js
+++ _site/assets/js/project.js
@@ -96,18 +96,22 @@ let amIOn = false;
 
 arrows.addEventListener("click", function(){
   let navMobile = document.querySelector(".navMobile");
+  let containerDiv = document.querySelector(".container");
+
 
 
   if (amIOn == false){
-  navMobile.classList.add("show");
-  arrows.innerHTML="–";
-  amIOn = true;
-  } else if (amIOn == true){
-  navMobile.classList.remove("show");
-  arrows.innerHTML="+";
-  amIOn = false;
-  }
-  console.log(amIOn);
+    navMobile.classList.add("show");
+    containerDiv.classList.add("hide");
+    arrows.innerHTML="–";
+    amIOn = true;
+    } else if (amIOn == true){
+    navMobile.classList.remove("show");
+    containerDiv.classList.remove("hide");
+    arrows.innerHTML="+";
+    amIOn = false;
+    }
+    console.log(amIOn);
 })
 
 // let amIOnDesktop = false;
